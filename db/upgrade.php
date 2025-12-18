85e9e7b09f2b62f95fca737ba18a0f81a3b4efda
allakutkina <seirenkioko@gmail.com>
2025-12-18 20:12:22 +0100

    Increment plugin version

--- db/upgrade.php
+++ db/upgrade.php
@@ -34,7 +34,7 @@ function xmldb_local_h5p_xapi_upgrade($oldversion) {
 
     $dbman = $DB->get_manager();
 
-    if ($oldversion < 2025091206) {
+    if ($oldversion < 2025121800) {
 
         // Define table local_h5p_xapi to be created.
         $table = new xmldb_table('local_h5p_xapi');
@@ -53,7 +53,7 @@ function xmldb_local_h5p_xapi_upgrade($oldversion) {
         }
 
         // Upgrade savepoint reached.
-        upgrade_plugin_savepoint(true, 2025091206, 'local', 'h5p_xapi');
+        upgrade_plugin_savepoint(true, 2025121800, 'local', 'h5p_xapi');
     }
 
     return true;
