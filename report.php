662fcde0c6d25d08581e95b4a52147f974116f42
allakutkina <seirenkioko@gmail.com>
2025-12-18 20:45:09 +0100

    Fixed report boilerplate

--- report.php
+++ report.php
@@ -1,5 +1,4 @@
 <?php
-// filepath: /home/kutkina/work/plugin/h5p_xapi/report.php
 // This file is part of Moodle - http://moodle.org/
 //
 // Moodle is free software: you can redistribute it and/or modify
